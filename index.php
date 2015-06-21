670563f06cc8d07b2fef25ebcd36b5cb8b1ec009
Beth Yue Shi <bethyueshi@Beths-MacBook-Air.local>
2015-06-20 18:56:24 -0700

    fix

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <script src="http://d3js.org/d3.geo.tile.v0.min.js"></script>
     <script src="http://d3js.org/topojson.v1.min.js"></script>
     <script src="http://libs.cartocdn.com/cartodb.js/v2/cartodb.js"></script>
+    <script>L_DISABLE_3D = true;</script>
     <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
     <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
         <!-- Bootstrap Core CSS -->
