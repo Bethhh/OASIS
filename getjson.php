9f45ac08d9d6b64a055711478440dbce95cef779
Beth Yue Shi <bethyueshi@Beths-MacBook-Air.local>
2016-05-03 23:26:58 -0700

    test test

--- getjson.php
+++ getjson.php
@@ -1,11 +1,10 @@
-</p>
-<?php
-$mysql_db_hostname = "localhost";
-$mysql_db_user = "root";
-$mysql_db_password = "12345";
-$mysql_db_database = "school";
 
-&nbsp;
+<?php
+$mysql_db_hostname = "http://52.53.177.54/";
+// $mysql_db_user = "root";
+// $mysql_db_password = "12345";
+// $mysql_db_database = "school";
+
 
 $con = @mysqli_connect($mysql_db_hostname, $mysql_db_user, $mysql_db_password,
  $mysql_db_database);
@@ -14,12 +13,11 @@ if (!$con) {
  trigger_error('Could not connect to MySQL: ' . mysqli_connect_error());
 }
 $var = array();
- $sql = "SELECT * FROM users";
+ $sql = "SELECT latitude, longitude, time FROM geo";
 $result = mysqli_query($con, $sql);
 
 while($obj = mysqli_fetch_object($result)) {
 $var[] = $obj;
 }
-echo '{"users":'.json_encode($var).'}';
+echo '{"geos":'.json_encode($var).'}';
 ?>
-<p style="text-align: justify;">
